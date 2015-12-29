a7b901e3989aba71545ce983f7bb024c15bbd007
Kuniaki IGARASHI <igaiga@gmail.com>
2015-12-29 18:13:41 +0900

    Add midnight working time sum

--- monthly_report.rb
+++ monthly_report.rb
@@ -20,29 +20,37 @@ class MonthlyReport
     HyphenTime.hyphen_time_sum(@data.map(&:working_times_sum))
   end
 
+  def midnight_working_times_sum
+    HyphenTime.hyphen_time_sum(@data.map(&:midnight_working_times_sum))
+  end
+
   def total_md
     result_lines = []
     result_lines << "#勤務時間集計"
     result_lines << "| 日付 | 時間 | 日合計 | 深夜時間合計 |"
     result_lines << "| --- | --- | --- | --- |"
-    weekly_working_min = 0 #週次集計
+    #週次集計
+    weekly_working_min = 0
+    weekly_midnight_working_min = 0
 
     (first_day..last_day).each do |date|
       daily_report = find(date)
       if daily_report
         result_lines << "|[#{date_string(date)}](#{daily_report.url})| #{daily_report.daily_working_time.to_hyphen.join ', '} | #{daily_report.working_times_sum} |#{daily_report.midnight_working_times_sum} |"
         weekly_working_min += HyphenTime.to_min(daily_report.working_times_sum)
+        weekly_midnight_working_min += HyphenTime.to_min(daily_report.midnight_working_times_sum)
       else
         result_lines << "|#{date_string(date)}| | | |"
       end
 
       if date.saturday? || (date == date.end_of_month) #週次集計
-        result_lines << "| |週合計| #{HyphenTime.to_hyphen(weekly_working_min)} | |"
+        result_lines << "| |週合計| #{HyphenTime.to_hyphen(weekly_working_min)} | #{HyphenTime.to_hyphen(weekly_midnight_working_min)} |"
         weekly_working_min = 0
+        weekly_midnight_working_min = 0
       end
     end
 
-    result_lines << "| 月合計 | | #{working_times_sum} | |"
+    result_lines << "| 月合計 | | #{working_times_sum} | #{midnight_working_times_sum} |"
 
     result_lines.join "\n"
   end
